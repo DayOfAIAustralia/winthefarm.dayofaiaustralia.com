ec8612fcfa96a03f7a2ddd14d02ece87c7475d1c
Wayne <Wayne@LAPTOP.localdomain>
2025-10-13 16:36:44 +1100

    Google tag manager hook and comptability for how to play steps on wider monitors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { DM_Serif_Text, Figtree } from "next/font/google";
+import Script from 'next/script'; // Import Script
+import GTMAnalytics from '@/components/GTMAnalytics'; // Import our new component
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -21,6 +23,8 @@ const figtree = Figtree({
   variable: "--font-figtree",
 });
 
+const GTM_ID = 'GTM-PQSWP4R8';
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -31,7 +35,35 @@ export default function RootLayout({
       lang="en"
       className={`${dmserif.variable} ${figtree.variable} antialiased`}
     >
-      <body>{children}</body>
+      {/* GTM Script */}
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `,
+        }}
+      />
+      <body>
+        {/* GTM Noscript */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        
+        <GTMAnalytics />
+
+        {children}
+      </body>
     </html>
   );
 }
---
+++ components/GTMAnalytics.tsx
@@ -0,0 +1,25 @@
+'use client'; // This is a client component
+
+import { usePathname, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+
+const pageview = (url: string) => {
+  if (typeof window.dataLayer !== 'undefined') {
+    window.dataLayer.push({
+      event: 'pageview',
+      page: url,
+    });
+  }
+};
+
+export default function GTMAnalytics() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const url = pathname + searchParams.toString();
+    pageview(url);
+  }, [pathname, searchParams]); // Fire effect on path or params change
+
+  return null;
+}
